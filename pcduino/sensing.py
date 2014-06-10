18478e58c39675c752058441cae018d5d7ec407e
Justin McBride <justin@dare599z.com>
2014-06-10 00:07:10 -0600

    added server and client python files

---
+++ pcduino/sensing.py
@@ -0,0 +1,74 @@
+import time, os
+import httplib, urllib
+import socket
+import sys
+import pickle
+
+ADC_PATH = os.path.normpath('/proc/')
+ADC_FILENAME = "adc"
+adcFiles = []
+
+boardName = 'justins'
+
+#headers = {}
+serverURL = "localhost"
+serverPort = 10363
+
+
+def delay(ms):
+	time.sleep(1.0*ms/1000)
+
+def setup():
+	#global headers
+	for i in range(0,6):
+		adcFiles.append(os.path.join(ADC_PATH, ADC_FILENAME+str(i)))
+	#headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+
+def loop(s):
+	while(1):
+		#updatePinReadings()
+		sendData(s, 15)
+		delay(5000)
+
+'''
+def upload(tempN, lightN):
+	postParams = urllib.urlencode({'@boardTag': boardName, '@temperature': tempN, 'light': lightN})
+	conn = httplib.HTTPConnection(serverURL)
+	conn.request("POST", "", postParams, headers)
+	response = conn.getresponse()
+	print response.status, response.reason
+	data = response.read()
+	print data
+	conn.close()
+'''
+
+def sendData(s, tempN):
+	packetD = {'tag' : boardName, 'temperature': tempN }
+	packetP = pickle.dumps(packetD)
+	s.send(packetP)
+
+def updatePinReadings():
+	for file in adcFiles:
+		fd = open(file, 'r')
+		fd.seek(0)
+		print "ADC Channel: " + str(adcFiles.index(file)) + " Result : " + int(f.read(32).split(':')[1].strip())
+		fd.close()
+
+def main():
+	setup()
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	except:
+		print 'Failed to create socket.'
+		sys.exit()
+	try:
+		remote_ip = socket.gethostbyname(serverURL)
+	except socket.gaierror:
+		print 'costname could not be resolved'
+		sys.exit()
+
+	s.connect((remote_ip, serverPort))
+	loop(s)
+
+if __name__ == "__main__":
+	main()
---
+++ server/server.py
@@ -0,0 +1,53 @@
+#created using http://www.binarytides.com/python-socket-server-code-example/
+
+import socket
+import sys
+import pickle
+from thread import *
+
+HOST = ''
+PORT = 10363
+
+def clientthread(conn):
+    #sending message to connected client
+    conn.send('connected to the server')
+
+    while True:
+        #receiving data from client
+        data = conn.recv(2048)
+        command = pickle.loads(data)
+        print "data[tag] = ", command['tag']
+        print "data[temperature] = ", command['temperature']
+        if not data:
+            break
+
+    conn.close()
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print 'Socket created'
+
+    #Bind socket to local host and port
+    try:
+        s.bind((HOST, PORT))
+    except:
+        print 'Bind failed.'
+        sys.exit()
+
+    print 'Socket bind complete'
+
+    #start listening on socket
+    s.listen(10)
+    print 'Socket now listening'
+
+    while 1:
+        #wait to accept a connection
+        conn, addr = s.accept()
+        print 'connected with ' + addr[0] + ':' + str(addr[1])
+        start_new_thread(clientthread, (conn,))
+
+    s.close()
+
+if __name__ == "__main__":
+    main()
+
