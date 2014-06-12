bb1168f93a7d1511822ff811dc2079db7d0b9ff2
Justin McBride <justin@dare599z.com>
2014-06-11 22:05:45 -0600

    Cleaned up sensing.py and added documentation

--- pcduino/sensing.py
+++ pcduino/sensing.py
@@ -1,61 +1,80 @@
 import time, os
-import httplib, urllib
+#import httplib, urllib
 import socket
 import sys
 import pickle
-from pcduino import analog_read
+from pypcduino import analog_read
 from pymongo import MongoClient
 
-ADC_PATH = os.path.normpath('/proc/')
-ADC_FILENAME = "adc"
-adcFiles = []
-boardName = 'justins'
+boardName = 'Justin'
 tempPinReading = 0
 lightPinReading = 0
 
-#headers = {}
 serverURL = 'localhost'
 #serverURL = 'mongodb://128.138.201.123:27017/'
 serverPort = 10363
 #mongoClient = MongoClient(serverURL)
 
+'''
+Put the program to sleep for a specified number
+of seconds
+'''
 def delay(ms):
 	time.sleep(1.0*ms/1000)
 
+'''
+Setup some objects or variables before we start the loop
+'''
 def setup():
+	#nothing to do right now
+
 	#global headers
-	for i in range(0,6):
-		adcFiles.append(os.path.join(ADC_PATH, ADC_FILENAME+str(i)))
 	#headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
 
+'''
+This is where the program will spend the majority
+of its time, looping through indefinitely.
+'''
 def loop(s):
 	while(1):
 		updatePinReadings()
 		sendData(s, tempPinReading)
 		delay(5000)
 
+'''
+Here we actually ship off the information to the server.
+We need to serialize the data to send it effectively (using
+pickle)
+'''
 def sendData(s, tempN):
 	packetD = {'tag' : boardName, 'temperature': tempN }
 	packetP = pickle.dumps(packetD)
 	s.send(packetP)
 
+'''
+This function will read the values reported by the hardware,
+and then save that data to the appropriate sensor's variable
+'''
 def updatePinReadings():
 	global tempPinReading
 	global lightPinReading
 	tempPinReading = analog_read(2)
 	lightPinReading = analog_read(4)
 
+'''
+The entry point of the program
+'''
 def main():
 	setup()
 	try:
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	except:
-		print 'Failed to create socket.'
+		print 'Failed to create a socket.'
 		sys.exit()
 	try:
 		remote_ip = socket.gethostbyname(serverURL)
 	except socket.gaierror:
-		print 'costname could not be resolved'
+		print 'Could not connect to the specified hostname.'
 		sys.exit()
 
 	s.connect((remote_ip, serverPort))
