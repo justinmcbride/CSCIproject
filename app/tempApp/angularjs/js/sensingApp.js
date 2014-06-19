90458795a3394839f32b87e7abf85a5f8d1b5555
ReedPluto <anderson.reed@gmail.com>
2014-06-18 20:36:38 -0600

    Angular with DreamFactory module

---
+++ app/tempApp/angularjs/js/sensingApp.js
@@ -0,0 +1,6 @@
+﻿angular.module("sensingTemp",['ngDreamFactory','customFilters'])
+	.constant('DSP_URL', dsp-csci-project.cloud.dreamfactory.com/files/applications/RemoteSensing/index.html)
+	.constant('DSP_API_KEY', X-DreamFactory-Application-Name)
+	.controller('sensingCtrl',['DreamFactory', function(DreamFactory)]){
+
+	}
