b4e11e53a82b9f66c4d51caab3973536455f7bbe
austruck <austincerny@gmail.com>
2014-06-23 08:53:05 -0600

    ide files
    all the android code

---
+++ android/test2-AndroidKickstartr/test2/src/com/test2/app/test2.java
@@ -0,0 +1,25 @@
+
+package com.test2.app;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class test2
+    extends SherlockActivity
+{
+
+
+    @AfterViews
+    void afterViews() {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getSupportMenuInflater();
+        return true;
+    }
+
+}
