47aad7050cce3714aab7a969370018cdaaac3d29
Your Name Here <anderson.reed@gmail.com>
2014-06-27 09:33:55 -0600

    Removed unused dependency files, moved JS from HTML

--- app/controllers/sensingApp.js
+++ app/controllers/sensingApp.js
@@ -1,7 +1,7 @@
 ﻿var DSP = "https://dsp-csci-project.cloud.dreamfactory.com/rest/mongodb/sensordata";
 var qString = "?app_name=TEST&fielbs=*";
 
-angular.module("sensingApp")
+angular.module("sensingApp",["customFilters"])
 	.controller('sensingCtrl', function($scope, $http){
     $http.get(DSP + qString).success(function(data) {
       $scope.data = data;
