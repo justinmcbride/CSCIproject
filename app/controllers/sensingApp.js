347a97d9f4778406648dd3e97598a22b6d2181a4
ReedPluto <anderson.reed@gmail.com>
2014-06-30 17:27:27 -0600

    Initial graph, with unfixed data combination

--- app/controllers/sensingApp.js
+++ app/controllers/sensingApp.js
@@ -1,10 +1,30 @@
 ﻿var DSP = "https://dsp-csci-project.cloud.dreamfactory.com/rest/mongodb/sensordata";
 var qString = "?app_name=TEST";
 
-angular.module("sensingApp",["customFilters"])
+angular.module("sensingApp",["customFilters","kendo.directives"])
 	.controller('sensingCtrl', function($scope, $http){
+    
     $http.get(DSP + qString).success(function(data) {
-      $scope.data = data;
+      $scope.data = data.record;
     });
+  
+  	$scope.chart = {
+    dataSource: {
+      data: $scope.data
+    },
+    title:"Temperature Recordings",
+    series: [{
+      field: 'sensorData.Temperature'
+    }],
+    seriesDefaults: {
+      type: 'area',
+      style: 'smooth'
+    }
+  };
+
+  $scope.$watchCollection('data', function(newData) {
+    $scope.chart.dataSource.data = newData;
+  });
+  
 });
 
