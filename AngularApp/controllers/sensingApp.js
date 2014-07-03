26fb96bdc4ff0ae6c885f65d98c8c1cfed1ebe29
Your Name Here <anderson.reed@gmail.com>
2014-07-02 22:18:52 -0600

    Further editing on comments

--- AngularApp/controllers/sensingApp.js
+++ AngularApp/controllers/sensingApp.js
@@ -1,29 +1,35 @@
+﻿// Main app controller for 'sensingApp', with 'custom.filter' and 'kendo.directives'
+// Defined variables include dreamfactory API, and required concatenation
 var DSP = "https://dsp-csci-project.cloud.dreamfactory.com/rest/mongodb/sensordata";
 var qString = "?app_name=RemoteSensing&limit=10&order=_id%20desc";
 
+
 angular.module("sensingApp",["customFilters","kendo.directives"])
+  
+  // Controller to define data for the $scope, after a get request to dreamfactory defined api
 	.controller('sensingCtrl', function($scope, $http){
     
+    // Get request to retrieving data and defining $scope.data
     $http.get(DSP + qString).success(function(data) {
       $scope.data = data.record;
     });
   
-  	// Custom variables within $scope.chart are used for manipulating kendo charts
+    // Custom variables within $scope.chart are used for manipulating kendo charts
   	$scope.chart = {
     dataSource: {
       data: $scope.data
     },
     title:"Temperature Recordings",
-    seriesDefaults: {
-      type: 'line',
-      style: 'smooth',
-      color:'blue'
-    },
     series: [{
       field: 'sensorData.Temperature'
-    }]
+    }],
+    seriesDefaults: {
+      type: 'area',
+      style: 'smooth'
+    }
   };
 
+  // watchCollection is required for the kendo charts
   $scope.$watchCollection('data', function(newData) {
     $scope.chart.dataSource.data = newData;
   });
